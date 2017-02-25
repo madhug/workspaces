30f76bb60b31cf9071ff7c38c53f7a7dbeef12d1
madhug <madhupramod@gmail.com>
2017-02-25 07:31:19 -0800

    removed linklocal

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
-import Workspaces from 'src/components/pages/workspaces';
+import Workspaces from './components/pages/workspaces';
 import rootReducer from './rootReducer';
 import './index.css';
 
